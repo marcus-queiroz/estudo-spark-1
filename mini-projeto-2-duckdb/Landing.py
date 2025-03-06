c34b173998b8179e5ce7e56f32ef534c04e9d369
Marcus Queiroz <mffpqueiroz@gmail.com>
2025-03-06 01:44:01 +0000

    refactor: Update file paths to use project-specific directory structure

--- mini-projeto-2-duckdb/Landing.py
+++ mini-projeto-2-duckdb/Landing.py
@@ -2,10 +2,10 @@ import shutil
 import os
 import glob
 
-arquivos_bs = glob.glob('C:\\lakehouse\\BikeStore\\*.csv')
-os.makedirs('data/landing/bike_store', exist_ok=True)
+arquivos_bs = glob.glob('mini-projeto-2-duckdb\\data\\BikeStore\\*.csv')
+os.makedirs('mini-projeto-2-duckdb/data/landing/', exist_ok=True)
 
 for arquivo in arquivos_bs:
     nome_arquivo = os.path.basename(arquivo)
-    destino = f'data/landing/bike_store/{nome_arquivo}'
+    destino = f'mini-projeto-2-duckdb/data/landing/{nome_arquivo}'
     shutil.copyfile(arquivo, destino)
